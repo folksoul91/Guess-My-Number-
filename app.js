28e2cd0f9e2c67582a134b995ef1bda60c333923
Dane Kim <mr.dane@danes-mbp.lan>
2022-06-07 20:19:25 -0400

    manipulating DOM to add styles to background and width when player wins

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ document.querySelector(".check").addEventListener("click", () => {
     document.querySelector(".message").textContent = "❌ No number!";
   } else if (guess === secretNumber) {
     document.querySelector(".message").textContent = "🎉 Correct Number!";
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".number").style.width = "30rem";
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector(".message").textContent = "📈 Too high!";
