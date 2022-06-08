84dd6a1506bc34852abd950d57f5669c3ab0ce26
Dane Kim <mr.dane@danes-mbp.lan>
2022-06-07 20:42:42 -0400

    implementing a click evt to Again btn and its logic

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 "use strict";
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-
-document.querySelector(".number").textContent = secretNumber;
 
 document.querySelector(".check").addEventListener("click", () => {
   const guess = Number(document.querySelector(".guess").value);
@@ -11,6 +9,7 @@ document.querySelector(".check").addEventListener("click", () => {
     document.querySelector(".message").textContent = "❌ No number!";
   } else if (guess === secretNumber) {
     document.querySelector(".message").textContent = "🎉 Correct Number!";
+    document.querySelector(".number").textContent = secretNumber;
     document.querySelector("body").style.backgroundColor = "#60b347";
     document.querySelector(".number").style.width = "30rem";
   } else if (guess > secretNumber) {
@@ -32,4 +31,15 @@ document.querySelector(".check").addEventListener("click", () => {
       document.querySelector(".score").textContent = 0;
     }
   }
+});
+
+document.querySelector(".again").addEventListener("click", () => {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector(".message").textContent = "Start guessing...";
+  document.querySelector(".score").textContent = score;
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".guess").value = "";
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector(".number").style.width = "15rem";
 });
